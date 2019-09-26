f295b338441f3e4f30fb887aa86e89ce616e394a
Octowl <alsaff1987@gmail.com>
2019-09-26 19:23:50 +0300

    Update syntax

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Components
 import ControlledForm from "./ControlledForm";
 import PeopleList from "./PeopleList";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="jumbotron mt-3 text-center">
-          <div className="row align-center mb-2">
-            <div className="col-4"></div>
-            <div className="col-4">
-              <h3 className="text">Tell us about yourself:</h3>
-            </div>
-            <div className="col-4"></div>
+function App() {
+  return (
+    <div className="container">
+      <div className="jumbotron mt-3 text-center">
+        <div className="row align-center mb-2">
+          <div className="col-4"></div>
+          <div className="col-4">
+            <h3 className="text">Tell us about yourself:</h3>
           </div>
-          <ControlledForm />
+          <div className="col-4"></div>
         </div>
-        <PeopleList />
+        <ControlledForm />
       </div>
-    );
-  }
+      <PeopleList />
+    </div>
+  );
 }
 
 export default App;
--- src/PeopleList.js
+++ src/PeopleList.js
@@ -1,30 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 
-class PersonCard extends Component {
-  render() {
-    const person = this.props.person;
-    return (
-      <div className="col-12 col-md-3">
-        <div className="card">
-          <div className="card-body">
-            <h3 className="card-title">{person.alias}</h3>
-            <p className="text-muted">{person.email}</p>
-            <p className="card-text">{person.description}</p>
-          </div>
+function PersonCard({ person }) {
+  return (
+    <div className="col-12 col-md-3">
+      <div className="card">
+        <div className="card-body">
+          <h3 className="card-title">{person.alias}</h3>
+          <p className="text-muted">{person.email}</p>
+          <p className="card-text">{person.description}</p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-class PeopleList extends Component {
-  render() {
-    const cards = this.props.people.map(person => (
-      <PersonCard key={person.alias} person={person} />
-    ));
-    return <div className="row my-5">{cards}</div>;
-  }
+function PeopleList({ people }) {
+  const cards = people.map(person => (
+    <PersonCard key={person.alias} person={person} />
+  ));
+  return <div className="row my-5">{cards}</div>;
 }
 
 const mapStateToProps = state => {
